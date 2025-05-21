72e7eee62510a85addfe77d46e261e5278c43bf8
chanduyadav23 <chanduy421@gmail.com>
2025-05-21 03:52:00 +0000

    e-comm-firstapi

---
+++ my-app/src/main.jsx
@@ -0,0 +1,40 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import App from './App.jsx'
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import About from './components/about.jsx'
+import Contact from './components/contact.jsx'
+let routes=createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    children:
+    [
+      {
+        path:'',
+        element:<Home/>
+      },
+        {
+        path:'/about',
+        element:<About/>
+      },
+        {
+        path:'/contact',
+        element:<Contact/>
+,      }
+    ]
+  }
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={routes}>
+    <App />
+    </RouterProvider>
+  </StrictMode>,
+)
